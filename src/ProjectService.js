334c4de2e1452b8c4d45325bd0a504bcf420d246
zeeshankhanminhas <127130521+zeeshankhanminhas@users.noreply.github.com>
2026-06-23 18:17:30 +0100

    Create controlled project service module

---
+++ src/ProjectService.js
@@ -0,0 +1,7 @@
+var MidtsProjectService = (function () {
+  function createProjectFromAcceptedQuote() {
+    return { ok:false, code:'NOT_IMPLEMENTED', message:'Project creation is not configured.' };
+  }
+
+  return { createProjectFromAcceptedQuote:createProjectFromAcceptedQuote };
+})();
